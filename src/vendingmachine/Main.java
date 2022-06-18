cf7f60d5c44526bf55b1a8d7c0e2a2020fb9495e
dhulas <supadhulas@gmail.com>
2022-06-18 14:59:29 +0200

    Commit skeleton and basis

---
+++ src/vendingmachine/Main.java
@@ -0,0 +1,32 @@
+package vendingmachine;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		VenMachine vendingMachine = null;
+		
+		
+		vendingMachine.displayProducts();
+		
+		String selectedProduct = scanner.nextLine();
+		int selectProductNumber = Integer.parseInt(selectedProduct);
+		
+		vendingMachine.selectProduct(selectProductNumber);
+		
+		vendingMachine.displayEnterCoinsMessage();
+		
+		String userEnteredCoins = scanner.nextLine();
+		
+		int[] enteredCoins = {}; //TODO
+		
+		vendingMachine.enterrCoins(enteredCoins);
+		vendingMachine.displayChangeMessage();
+				
+	
+	}
+
+}
